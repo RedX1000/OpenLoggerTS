c5ae3b66f13852026dc800942e772fb9a5e1105a
RedX1000 <clwms16@gmail.com>
2022-03-26 20:37:33 -0500

    Alt1 can see and highlight the window

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@
 import * as a1lib from "@alt1/base";
 import { ImgRef } from "@alt1/base";
 import * as resemble from "resemblejs";
-import * as lsdb from '../JSONs/LocalStorageInit.json'
-import * as items from '../JSONs/ItemsAndImages.json'
+import * as lsdb from '../JSONs/LocalStorageInit.json';
+import * as items from '../JSONs/ItemsAndImages.json';
+import * as fs from "fs";
 
 //tell webpack to add index.html and appconfig.json to output
 require("!file-loader?name=[name].[ext]!./index.html");
@@ -48,26 +49,28 @@ export function changeClueTierSpan(id){
 	localStorage.setItem("Checked button", id);
 }
 
+var rewardSlots = ["first_item", "second_item", "third_item", "fourth_item", "fifth_item", "sixth_item", "seventh_item", "eigth_item", "ninth_item"]
+
 //loads all images as raw pixel data async, images have to be saved as *.data.png
 //this also takes care of metadata headers in the image that make browser load the image
 //with slightly wrong colors
 //this function is async, so you cant acccess the images instantly but generally takes <20ms
 //use `await imgs.promise` if you want to use the images as soon as they are loaded
 var imgs = a1lib.ImageDetect.webpackImages({
-	homeport: require("../images/TrailComplete.data.png")
+	trailComplete: require("../images/TrailComplete.data.png")
 });
 
 //listen for pasted (ctrl-v) images, usually used in the browser version of an app
 a1lib.PasteInput.listen(img => {
-	findHomeport(img);
+	findtrailComplete(img);
 }, (err, errid) => {
+	
 	//output.insertAdjacentHTML("beforeend", `<div><b>${errid}</b>  ${err}</div>`);
 });
 
 //You can reach exports on window.TEST because of
 //config.makeUmd("testpackage", "TEST"); in webpack.config.ts
 export function capture() {
-	console.log("in capture")
 	if (!window.alt1) {
 		//output.insertAdjacentHTML("beforeend", `<div>You need to run this page in alt1 to capture the screen</div>`);
 		return;
@@ -77,25 +80,48 @@ export function capture() {
 		return;
 	}
 	var img = a1lib.captureHoldFullRs();
-	findHomeport(img);
+	findtrailComplete(img);
 }
 
-function findHomeport(img: ImgRef) {
-	var loc = img.findSubimage(imgs.homeport);
-	//output.insertAdjacentHTML("beforeend", `<div>homeport matches: ${JSON.stringify(loc)}</div>`);
+function findtrailComplete(img: ImgRef) {
+	var loc = img.findSubimage(imgs.trailComplete);
 
 	//overlay the result on screen if running in alt1
 	if (window.alt1) {
 		if (loc.length != 0) {
-			alt1.overLayRect(a1lib.mixColor(255, 255, 255), loc[0].x, loc[0].y, imgs.homeport.width, imgs.homeport.height, 2000, 3);
+			alt1.overLayRect(a1lib.mixColor(255,0,0), loc[0].x - 27, loc[0].y - 13, imgs.trailComplete.width + 278, imgs.trailComplete.height + 213, 2000, 3);
 		} else {
-			alt1.overLayTextEx("Couldn't find homeport button", a1lib.mixColor(255, 255, 255), 20, Math.round(alt1.rsWidth / 2), 200, 2000, "", true, true);
+			alt1.overLayTextEx("Couldn't find Rewards window. Remove\n    any obstructions or open a casket.", a1lib.mixColor(255, 255, 255), 20, Math.round(alt1.rsWidth / 2), 200, 2000, "", true, true);
 		}
 	}
 
-	//get raw pixels of image and show on screen (used mostly for debug)
-	var buf = img.toData(100, 100, 200, 200);
-	buf.show();
+	//get raw pixels of image and show on screen (used mostly for debug)  
+	var buf = img.toData(loc[0].x - 27, loc[0].y - 13, imgs.trailComplete.width + 278, imgs.trailComplete.height + 213);
+	//buf.show();
+
+	console.log("About to run array...")
+	let crops = new Array<ImageData>(9)
+	var x1 = loc[0].x - 1
+	var y1 = loc[0].y + 39
+	var x2 = loc[0].x + 1
+	var y2 = loc[0].y - 136
+	for(let i = 0; i < crops.length; i++) {
+		console.log("In array check...")
+		crops[i] = img.toData(x1, y1, x2, y2,);
+		alt1.overLayRect(a1lib.mixColor(255,144,0), x1, y1, x2, y2, 2000, 1);
+		
+		//var imgcanvas = document.createElement('canvas')
+		//var context = imgcanvas.getContext('2d')
+		//var imgvar = document.getElementById(rewardSlots[i])
+		//imgcanvas.width = 32;
+		//imgcanvas.height = 32;
+		//context.drawImage(crops[i], 0 , 0)
+
+		//imgvar.setAttribute('img', crops[i])
+		x1 += 40
+		crops[i].show()
+	}
+
 }
 
 //print text world
